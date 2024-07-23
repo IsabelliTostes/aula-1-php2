21d4b61d2d8332724037811a393716dfaa4e691c
IsabelliTostes <isa_tostes@hotmail.com>
2024-07-23 17:13:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+!<!DOCTYPE html>
+    <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+            <?php 
+                /*2 – Escreva um algoritmo que leia três números nas variáveis Val1, Val2
+                e Val3, calcule sua média na variável media e exiba para o usuário o
+                resultado*/
+
+                $Portugues= 80;
+                $Matematica= 70;
+                $Historia= 80;
+                $Fisica=60;
+                $Educacao_fisica= 50;
+                $Sociologia= 80;
+                $Geografia= 70;
+                $Filosofia= 70;
+                $Ensino_Religioso= 80;
+                $Biologia= 70;
+                $Media= 71;
+                
+                    echo 'Media 71 aprovada';
+                
+            ?>
+
+        </body>
+
+</html>
